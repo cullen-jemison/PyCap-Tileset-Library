a77d7e7a79aa7046967d1cc7cbbc523e8cf5ef2b
Cullen <cullenjski@gmail.com>
2013-03-06 23:02:27 -0500

    Finished basic tile engine code
    TODO:
    Add image texturing
    Add damaging and destruction of tiles

--- game.py
+++ game.py
@@ -16,18 +16,69 @@ import Pycap as PC
 PCR = None
 import tile as TS
 
+KEYLEFT		= 65
+KEYRIGHT	= 68
+KEYUP		= 87
+KEYDOWN		= 83
+
+leftDown = 0
+rightDown = 0
+upDown = 0
+downDown = 0
+
+scrollX = 100
+scrollY = 100
+
 def loadBase():
 	import PycapRes
 	global PCR
 	PCR = PycapRes
 
 def init():
-	map = TS.Map()
-	map.loadTileset("Res\\Tilesets\\Tileset.tsm")
-	print map.getTiles()
+	global map
+	map = TS.Map("Res\\Tilesets\\Tileset.tsm")
+	map.buildMap()
 
 def update( delta ):
+	global scrollX
+	global scrollY
+	
 	PC.markDirty()
+	
+	scrollX += (rightDown - leftDown) * 2
+	scrollY += (downDown - upDown) * 2
+	
+	map.update(delta, scrollX, scrollY)
 
 def draw():
-	pass
+	PC.setColour(255, 255, 255, 255)
+	PC.fillRect(0, 0, 800, 600)
+	map.draw()
+	
+def keyDown(key):
+	if key == KEYLEFT:
+		global leftDown
+		leftDown = 1
+	elif key == KEYRIGHT:
+		global rightDown
+		rightDown = 1
+	elif key == KEYUP:
+		global upDown
+		upDown = 1
+	elif key == KEYDOWN:
+		global downDown
+		downDown = 1 
+
+def keyUp(key):
+        if key == KEYLEFT:
+		global leftDown
+		leftDown = 0
+	elif key == KEYRIGHT:
+		global rightDown
+		rightDown = 0
+	elif key == KEYUP:
+		global upDown
+		upDown = 0
+	elif key == KEYDOWN:
+		global downDown
+		downDown = 0
--- tile.py
+++ tile.py
@@ -1,37 +1,102 @@
+""" 
+	Pycap Tileset Engine
+	copyright (c) Cullen Jemison 2013
+"""
+
 from ConfigObj import ConfigObj
+import Pycap as PC
 
 class Map:
-	def __init__(self):
-		map = 0
-		tiles = 0
-		tsm = 0
-		aliases = 0
-		metadata = 0
-		Tiles = 0
-		
-	def loadTileset(self, f):
+	def __init__(self, f):
 		# load the map file
-		self.tsm = ConfigObj(f)
+		tsm = ConfigObj(f)
 		
-		# construct the tiles array for use later
-		self.tiles = self.tsm['tileset']
+		# construct the tiles dictionary
+		self.tiles = tsm['tileset']
 		
 		# load map array
-		self.map = self.tsm['map']['tiles'].split()
+		self.map = tsm['map']['tiles'].split()
 		
-		# fill up the alias array
-		self.aliases = []
-		for i in self.tiles:
-			self.aliases[i[alias]] = i['tile'])
+		# construct the alias dictionary
+		tilenames = tsm['tileset'].keys() # obtain a list of tile names
+		self.aliases = {}
+		for i in tilenames:
+			self.aliases[tsm['tileset'][i]['alias']] = i
 			
 		# load explicit map metadata
-		self.metadata = [self.tsm['map']['width'], self.tsm['map']['height'], self.tsm['map']['scale'], self.tsm['map']['size']]
+		self.metadata = tsm['map']
+		del self.metadata['tiles']
 		
 	def buildMap(self):
 		# create the tile entities that will make up the map
-		Tiles = []
-		for i in self.map:
-			for a in self.aliases:
-				if a.keys()
-			Tiles.append(Tile(self.tiles[])
+		self.Tiles = []
+		for j in self.map:
+			self.Tiles.append(Tile(self.tiles[self.aliases[j]]))
+			
+	def update(self, delta, sx, sy):
+		# update all of the Tile locations from global scrolling variables
+		column = 0
+		row = 0
+		for i in self.Tiles:
+			i.update(delta, sx + int(self.metadata['size'])* row, sy + int(self.metadata['size']) * column, int(self.metadata['size']))
+			row += 1
+			if row > int(self.metadata['width'])-1:
+				column += 1
+				row = 0
+			
+	def draw(self):
+		for i in self.Tiles:
+			i.draw()
+		
+			
+class Tile:
+	def __init__(self, d):
+		self.definition = d
+		self.x = 0
+		self.y = 0
+		self.s = 5
+		
+	def update(self, delta, x, y, s):
+		self.x = x
+		self.y = y
+		self.s = s
+		
+	def draw(self):
+		if self.definition.has_key('image'):
+			print "image tiles not yet supported"
+		elif self.definition.has_key('color'):
+			PC.setColour(TileColors[self.definition['color']]['red'], TileColors[self.definition['color']]['green'], TileColors[self.definition['color']]['blue'], 255)
+			PC.fillRect(self.x, self.y, self.s, self.s)
+		else:
+			print "tile must have color or image attribute"
+			
+			
+TileColors = {
+	'black': {
+		'red': 0,
+		'green': 0,
+		'blue': 0,
+		},
+	'white': {
+		'red': 255,
+		'green': 255,
+		'blue': 255,
+		},
+	'red': {
+		'red': 255,
+		'green': 0,
+		'blue': 0,
+		},
+	'green': {
+		'red': 0,
+		'green': 255,
+		'blue': 0,
+		},
+	'blue': {
+		'red': 0,
+		'green': 0,
+		'blue': 255,
+		}
+	}
+	
 		
